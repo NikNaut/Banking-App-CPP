cf95019bbda30ac6d6218abceb997fad36263d66
NikNaut <nikunjwahie@gmail.com>
2023-12-14 02:12:26 +0530

    Revert "Revert "file added for project""
    This reverts commit d42635f650bb390b4bee7a950dcb4e229c405c0e.

---
+++ banking_app.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+void depositMoney(double amount);
+void withdrawMoney(double amount);
+void checkBalance();
+
+
+int main() {
+    depositMoney();
+    return 0;
+}
+
+void depositMoney(double amount) {
+    std::cout<<"from deposit money";
+}
